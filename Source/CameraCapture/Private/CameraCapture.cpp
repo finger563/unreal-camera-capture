c020b87c15d5e2dddd279b3631b17e143295fd27
William Emfinger <waemfinger@gmail.com>
2026-02-19 13:18:08 -0600

    feat: Add distortion shaders for simple lens distortion

--- Source/CameraCapture/Private/CameraCapture.cpp
+++ Source/CameraCapture/Private/CameraCapture.cpp
@@ -1,23 +1,32 @@
 // Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
 
 #include "CameraCapture.h"
-#include "Core.h"
+#include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
-#include "Interfaces/IPluginManager.h"
+#include "ShaderCore.h"
 
 #define LOCTEXT_NAMESPACE "FCameraCaptureModule"
 
 void FCameraCaptureModule::StartupModule()
 {
-	// This code will execute after your module is loaded into memory; the exact
-	// timing is specified in the .uplugin file per-module
+	// Register the plugin's Shaders/ directory as a virtual shader include path.
+	// This lets Custom HLSL nodes in materials use: #include "/CameraCapture/Private/LensDistortion.usf"
+	FString PluginShaderDir = FPaths::Combine(FPaths::ProjectPluginsDir(), TEXT("CameraCapture"), TEXT("Shaders"));
+	FPaths::CollapseRelativeDirectories(PluginShaderDir);
+
+	if (FPaths::DirectoryExists(PluginShaderDir))
+	{
+		AddShaderSourceDirectoryMapping(TEXT("/CameraCapture"), PluginShaderDir);
+		UE_LOG(LogTemp, Log, TEXT("CameraCapture: Registered shader source directory: %s"), *PluginShaderDir);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("CameraCapture: Shader directory not found at: %s"), *PluginShaderDir);
+	}
 }
 
 void FCameraCaptureModule::ShutdownModule()
 {
-	// This function may be called during shutdown to clean up your module. For
-	// modules that support dynamic reloading, we call this function before
-	// unloading the module.
 }
 
 #undef LOCTEXT_NAMESPACE
